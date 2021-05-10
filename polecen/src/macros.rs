3764a12e1c1a1a2ebb04c58ca869a14163ec25eb
Unoqwy <julien.qwy@gmail.com>
2021-05-10 21:50:04 +0200

    replace ident by expr for read_args macro

--- polecen/src/macros.rs
+++ polecen/src/macros.rs
@@ -1,13 +1,13 @@
 #[macro_export]
 macro_rules! read_args {
-    ($ty:ty, $args:ident, $ctx:ident, $guild_id:expr) => {
+    ($ty:ty, $args:expr, $ctx:expr, $guild_id:expr) => {
         <$ty>::read_arguments(
             $args,
             ::polecen::arguments::parse::ArgumentParseContext::new($ctx, $guild_id),
         )
         .await
     };
-    ($ty:ty, $args:ident, $ctx:ident, [M] $message:ident) => {
+    ($ty:ty, $args:expr, $ctx:expr, [M] $message:ident) => {
         ::polecen::read_args!($ty, $args, $ctx, $message.guild_id)
     };
 }
